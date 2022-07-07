7762a3f93626a5b6e7e9189f5ca59326486c81b4
VitalikMelnyk <melnyk.vitalikk@gmail.com>
2022-07-07 11:24:43 +0300

    feat: add slug and title for home page

--- app/schema/single-document/HomePage/index.js
+++ app/schema/single-document/HomePage/index.js
@@ -4,6 +4,21 @@ export default {
   title: 'Home Page',
   fields: [
     {
+      name: 'title',
+      type: 'string',
+      title: 'Title',
+    },
+    {
+      name: 'slug',
+      type: 'slug',
+      // options: {
+      //   source: 'title',
+      //   maxLength: 96,
+      // },
+      validation: (Rule) => Rule.required(),
+    },
+
+    {
       name: 'introSlider',
       type: 'intro-slider',
       title: 'Intro Slider',
