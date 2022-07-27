5e749dac39e5adbaea3d91648d4d820602d4c6dd
VitalikMelnyk <melnyk.vitalikk@gmail.com>
2022-07-27 15:38:55 +0300

    feat: add updates-subscriber schema

---
+++ app/schema/documents/UpdatesSubscriber/index.js
@@ -0,0 +1,13 @@
+export default {
+  name: 'updates-subscriber',
+  type: 'document',
+  title: 'Updates Subscriber',
+  fields: [
+    {
+      name: 'email',
+      type: 'string',
+      title: 'Email',
+      readOnly: true,
+    },
+  ],
+};
