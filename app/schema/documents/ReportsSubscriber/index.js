54f3c746b2d984d6f8fd0ab0147884aaa2f1eb18
VitalikMelnyk <melnyk.vitalikk@gmail.com>
2022-07-27 15:38:28 +0300

    feat: add reports-subscriber schema

---
+++ app/schema/documents/ReportsSubscriber/index.js
@@ -0,0 +1,13 @@
+export default {
+  name: 'reports-subscriber',
+  type: 'document',
+  title: 'Reports Subscriber',
+  fields: [
+    {
+      name: 'email',
+      type: 'string',
+      title: 'Email',
+      readOnly: true,
+    },
+  ],
+};
